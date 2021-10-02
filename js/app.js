c11b8c94fda0f516430816dd7d613373c1f9180d
usigna <bukazaokna@gmail.com>
2021-10-02 20:00:56 +0200

    Add template

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
-function showBtnAnimation() {
-  const buttons = document.querySelectorAll('.btn');
-
-  buttons.forEach(btn => {
-    btn.addEventListener('mouseover', function(e) {
-      let x = e.clientX - e.target.offsetLeft;
-      let y = e.clientY - e.target.offsetTop;
-    
-      let ripples = document.createElement('span');
-      ripples.style.left = x + 'px';
-      ripples.style.top = y + 'px';
-      this.appendChild(ripples);
-      
-      setTimeout(() => {
-        ripples.remove();
-      }, 1000);
-    });
-  })
-}
-
 const init = function () {
-  // showBtnAnimation();
+
 };
 
 document.addEventListener('DOMContentLoaded', init);
